ee5bfd58443e5e2124746825b615488977b8b6bb
Jeff Xu <jiaxijeffxu@gmail.com>
2021-06-05 17:16:12 -0700

    able to get all records

--- airtable.js
+++ airtable.js
@@ -1,27 +1,25 @@
-// const Airtable = require("airtable");
 import airtable from "airtable";
-var base = new airtable({apiKey: 'process_env.API_KEY'}).base('process_env.API_BASE');
 
-export function getAllEvents() {
+var base = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_BASE);
+
+export function getAllEvents(req, res) {
+    var events = new Array();
     base('Event Calendar').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
     
         records.forEach(function(record) {
-            console.log('Retrieved', record.get('Name'));
+            events.push(record['fields']);
         });
     
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
         fetchNextPage();
     
     }, function done(err) {
-        if (err) { console.error(err); return; }
+        if (err) { 
+            console.error(err); 
+            return; 
+        } else {
+            res.json(events);
+        }
     });
 }
-
-// export { getAllEvents };
--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send("Welcome to CMU T&E"));
 
-app.get('/events', (req, res) => res.send(getAllEvents()));
+app.get('/events', (req, res) => getAllEvents(req, res));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
