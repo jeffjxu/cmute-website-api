243ef39a02b7b3270a3045d85598bf3b5a23c53d
Jeff Xu <jiaxijeffxu@gmail.com>
2021-06-07 15:41:32 -0700

    grab events from event series table

--- airtable.js
+++ airtable.js
@@ -1,13 +1,36 @@
 import airtable from "airtable";
 
-var base = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_BASE);
+var pastEventsBase = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_PAST_EVENTS_BASE);
+var newEventsBase = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_NEW_EVENTS_BASE);
 
-export function getAllEvents(req, res) {
+export function getPastEvents(req, res) {
     var events = new Array();
-    base('Event Calendar').select({
+    pastEventsBase('Event Calendar').select({
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
     
+        records.forEach(function(record) {
+            events.push(record['fields']);
+        });
+    
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { 
+            console.error(err); 
+            return; 
+        } else {
+            res.json(events);
+        }
+    });
+}
+
+export function getNewEvents(req, res) {
+    var events = new Array();
+    newEventsBase('Marketing Requests MASTER').select({
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+
         records.forEach(function(record) {
             events.push(record['fields']);
         });
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getAllEvents } from './airtable.js';
+import { getPastEvents, getNewEvents } from './airtable.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send("Welcome to CMU T&E"));
 
-app.get('/events', (req, res) => getAllEvents(req, res));
+app.get('/events', (req, res) => getPastEvents(req, res));
+
+app.get('/newEvents', (req, res) => getNewEvents(req, res));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
