e4ebe8c47a59fc2e4bb492d3436a67cf06e02de9
Jeff Xu <jiaxijeffxu@gmail.com>
2021-06-14 17:34:31 -0700

    combine 2 tables and add filter for event series

--- airtable.js
+++ airtable.js
@@ -3,11 +3,11 @@ import airtable from "airtable";
 var pastEventsBase = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_PAST_EVENTS_BASE);
 var newEventsBase = new airtable({apiKey: process.env.API_KEY}).base(process.env.API_NEW_EVENTS_BASE);
 
+// get all event from Event Calendar table and return to user
 export function getPastEvents(req, res) {
     var events = new Array();
-    pastEventsBase('Event Calendar').select({
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
+    pastEventsBase('Event Calendar').select()
+    .eachPage(function page(records, fetchNextPage) {
     
         records.forEach(function(record) {
             events.push(record['fields']);
@@ -25,10 +25,76 @@ export function getPastEvents(req, res) {
     });
 }
 
+// get all events from Marketing Request MASTER table and return to user
 export function getNewEvents(req, res) {
     var events = new Array();
+    newEventsBase('Marketing Requests MASTER').select()
+    .eachPage(function page(records, fetchNextPage) {
+
+        records.forEach(function(record) {
+            events.push(record['fields']);
+        });
+    
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { 
+            console.error(err); 
+            return; 
+        } else {
+            res.json(events);
+        }
+    });
+}
+
+// get all events from Marketing Request MASTER table and 
+// Event Calendar table and return to user
+export function getAllEvents(req, res) {
+    var events = new Array();
+    newEventsBase('Marketing Requests MASTER').select()
+    .eachPage(function page(records, fetchNextPage) {
+
+        records.forEach(function(record) {
+            events.push(record['fields']);
+        });
+    
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { 
+            console.error(err); 
+            return; 
+        } else {
+            pastEventsBase('Event Calendar').select({
+                view: "Grid view"
+            }).eachPage(function page(records, fetchNextPage) {
+            
+                records.forEach(function(record) {
+                    events.push(record['fields']);
+                });
+            
+                fetchNextPage();
+            
+            }, function done(err) {
+                if (err) { 
+                    console.error(err); 
+                    return; 
+                } else {
+                    res.json(events);
+                }
+            });
+        }
+    });
+}
+
+// filter events from Marketing Request MASTER by event series
+// and return to user
+export function eventSeries(req, res) {
+    var events = new Array();
+    var eventSerie = req.query.eventSerie;
     newEventsBase('Marketing Requests MASTER').select({
-        view: "Grid view"
+        view: "Grid view",
+        filterByFormula: `{Event Series} = '${eventSerie}'`
     }).eachPage(function page(records, fetchNextPage) {
 
         records.forEach(function(record) {
--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from 'express';
-import { getPastEvents, getNewEvents } from './airtable.js';
+import bodyParser from 'body-parser';
+
+import { getPastEvents, getNewEvents, getAllEvents, eventSeries } from './airtable.js';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send("Welcome to CMU T&E"));
 
-app.get('/events', (req, res) => getPastEvents(req, res));
+app.get('/events', (req, res) => getAllEvents(req, res));
+
+app.get('/pastEvents', (req, res) => getPastEvents(req, res));
 
 app.get('/newEvents', (req, res) => getNewEvents(req, res));
+
+app.get('/eventSeries', (req, res) => eventSeries(req, res));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
