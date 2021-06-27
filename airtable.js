0ee8640c53ed75563d7023bd9410bc9dbd67fac8
Jeff Xu <jiaxijeffxu@gmail.com>
2021-06-26 17:49:58 -0700

    add search

--- airtable.js
+++ airtable.js
@@ -112,3 +112,66 @@ export function eventSeries(req, res) {
         }
     });
 }
+
+// search for events based on a term from Marketing Request MASTER
+// and Event Calendar tables and return to user
+export function searchEvents(req, res) {
+    var events = new Array();
+    var term = req.query.search;
+    newEventsBase('Marketing Requests MASTER').select()
+    .eachPage(function page(records, fetchNextPage) {
+
+        records.forEach(function(record) {
+            if (searchNewEvents(term, record['fields'])) {
+                events.push(record['fields']);
+            }
+        });
+    
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { 
+            console.error(err); 
+            return; 
+        } else {
+            pastEventsBase('Event Calendar').select({
+                view: "Grid view"
+            }).eachPage(function page(records, fetchNextPage) {
+            
+                records.forEach(function(record) {
+                    if (searchOldEvents(term, record['fields'])) {
+                        events.push(record['fields']);
+                    }
+                });
+            
+                fetchNextPage();
+            
+            }, function done(err) {
+                if (err) { 
+                    console.error(err); 
+                    return; 
+                } else {
+                    res.json(events);
+                }
+            });
+        }
+    });
+}
+
+export function searchNewEvents(term, event) {
+    term = term.toUpperCase();
+
+    return (event['Event Title'] != null && event['Event Title'].toUpperCase().includes(term)) ||
+        (event['Event Type'] != null && event['Event Type'][0].toUpperCase().includes(term)) ||
+        (event['Event Series'] != null && event['Event Series'].toUpperCase().includes(term)) ||
+        (event['Event Description'] != null && event['Event Description'].toUpperCase().includes(term)) ||
+        (event['Speaker Info'] != null && event['Speaker Info'].toUpperCase().includes(term)) ||
+        (event['Audience'] != null && event['Audience'].toUpperCase().includes(term));
+}
+
+export function searchOldEvents(term, event) {
+    term = term.toUpperCase();
+
+    return (event['Name'] != null && event['Name'].toUpperCase().includes(term)) ||
+        (event['Notes'] != null && event['Notes'].toUpperCase().include(term));
+}
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import { getPastEvents, getNewEvents, getAllEvents, eventSeries } from './airtable.js';
+import { 
+    getPastEvents,
+    getNewEvents,
+    getAllEvents,
+    eventSeries, 
+    searchEvents
+} from './airtable.js';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,5 +24,7 @@ app.get('/pastEvents', (req, res) => getPastEvents(req, res));
 app.get('/newEvents', (req, res) => getNewEvents(req, res));
 
 app.get('/eventSeries', (req, res) => eventSeries(req, res));
+
+app.get('/search', (req, res) => searchEvents(req, res));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
